ead88551c08e1cd187fc0566c88fcd1169e83619
Tabarak7 <167092658+Tabarak7@users.noreply.github.com>
2024-05-03 17:23:23 +0200

    +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,47 @@
     }; 
   
 </script>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+        background-color: #b3d2d3;
+        font-family: "Abril Fatface", serif; }
+    .login-box {
+        background-color: #FFFFFF;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 2px 2px 15px rgba(0,0,0,0.1);
+        width: 300px; }
+    .login-box h1 {
+        color: #333;
+        text-align: center;
+        margin-bottom: 28px; }
+    .form-field {
+        width: 90%;
+        padding: 10px;
+        margin-bottom: 16px;
+        border: 1px solid #ccc;
+        border-radius: 4px; }
+    .login-button {
+        width: 100%;
+        padding: 10px;
+        background-color: #b3d2d3;
+        color: white;
+        border-radius: 4px;
+        cursor: pointer; }
+    .link {
+        display: block;
+        margin-top: 5px;
+        text-align: center;
+        color: #5CACEE;
+        text-decoration: none; }
+</style>
+
 <h1>Diabeat </h1>
 <div>
 {input}
